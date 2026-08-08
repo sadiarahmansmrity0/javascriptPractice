3236716303508cb9caee1ffed9f6b440e95f2ed3
sadiarahmansmrity0 <sadiarahmansmrity9@gmail.com>
2026-08-08 21:52:11 +0600

    docs: add exercise heading comment

--- practice-exercises/chapter-7/exercise-4.js
+++ practice-exercises/chapter-7/exercise-4.js
@@ -1,5 +1,5 @@
 //4. Write a function that takes an array of product objects and returns the total stock using reduce.
-
+// Exercise 4: Calculate total inventory stock using Array.prototype.reduce()
 const inventory = [
     { product: "Notebook", stock: 40 },
     { product: "Gel Pen", stock: 100 },
