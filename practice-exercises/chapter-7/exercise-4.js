7888ee0d46821a36ddee4578bac986be86c033a6
sadiarahmansmrity0 <sadiarahmansmrity9@gmail.com>
2026-08-08 21:54:26 +0600

    refactor: rename reduce accumulator variables for readability

--- practice-exercises/chapter-7/exercise-4.js
+++ practice-exercises/chapter-7/exercise-4.js
@@ -6,5 +6,5 @@ const inventory = [
     { product: "Backpack", stock: 7 }
 ];
 
-const getTotalStock = (items) => items.reduce((sum, item) => sum + item.stock, 0);
+const getTotalStock = (items) => items.reduce((total, currentItem) => total + currentItem.stock, 0);
 console.log("Total Items in Stock:", getTotalStock(inventory)); // Output: 147
